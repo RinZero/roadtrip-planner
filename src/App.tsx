6c3539e1eda0864e513148adf46a75d4839cca55
Jonathan <jonathan@lex.or.at>
2020-11-27 18:43:37 +0100

    implement the here map with test data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { DisplayMapClass } from "./DisplayMapClass";
+import { DisplayMapFC } from "./DisplayMapFC";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DisplayMapFC />
     </div>
   );
 }

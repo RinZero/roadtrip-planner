2c3eb37387226d35d335fab3f7f3c6fa465a068b
Jonathan <jonathan@lex.or.at>
2021-01-31 21:09:13 +0100

    feat(utils): add basic fetch + object classes

---
+++ src/utils/fetchHereData.ts
@@ -0,0 +1,47 @@
+export type HereApiDiscover = {
+  endpoint: 'discover'
+  query?: string
+}
+
+export type HereApiBrowse = {
+  endpoint: 'browse'
+  categories: string
+  name: string
+  query?: string
+}
+
+export type HereApiParams = {
+  object: HereApiDiscover | HereApiBrowse
+  at: { longitude: number; latitude: number }
+  limit: number
+  language?: string
+  show?: string[]
+  //TODO: add route param
+}
+
+export const fetchHereData = async (props: HereApiParams) => {
+  const { object, at, limit, language, show } = props
+  const url =
+    'https://' +
+    object.endpoint +
+    '.search.hereapi.com/v1/' +
+    object.endpoint +
+    '?at=' +
+    at.longitude.toString() +
+    ',' +
+    at.latitude.toString() +
+    '&limit=' +
+    limit.toString() +
+    '&lang=' +
+    language +
+    '&apiKey=E2lDYLhdeOT8rv2atmJ78m7_jafCkXg3NmgSAwjpcdE' +
+    '&q=' +
+    object.query
+  //'https://discover.search.hereapi.com/v1/discover?at=52.8173086,12.2368342&limit=5&lang=en&q=Obi+Hamburg&apiKey=E2lDYLhdeOT8rv2atmJ78m7_jafCkXg3NmgSAwjpcdE'
+  // https://browse.search.hereapi.com/v1/browse
+  const response = await fetch(url)
+  const data = await response.json()
+  // eslint-disable-next-line no-console
+  console.log(data)
+  return data
+}

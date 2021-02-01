4ee9831097bd1026aaafe6691a559fb6f759df6b
Jonathan <jonathan@lex.or.at>
2021-02-01 11:15:55 +0100

    feat(roadtripform): add switiching functionality

--- src/components/StepsMenu/StepsMenu.tsx
+++ src/components/StepsMenu/StepsMenu.tsx
@@ -2,7 +2,10 @@
 import React, { memo } from 'react'
 
 import { Box, Fab, Typography } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
+
+import { setProgressStep } from '../../store/actions'
 
 const StyledFab = styled(Fab)`
   background-color: #fff;
@@ -10,25 +13,26 @@ const StyledFab = styled(Fab)`
 `
 
 const StepsMenu = () => {
+  const dispatch = useDispatch()
   return (
     <Box display="flex" m={2} width="100%" justifyContent="space-evenly">
       <StyledFab
-        onClick={() => console.log('placeholder for store dispatch 1')}
+        onClick={() => dispatch(setProgressStep({ progressStep: '1' }))}
       >
         <Typography variant="h5">1</Typography>
       </StyledFab>
       <StyledFab
-        onClick={() => console.log('placeholder for store dispatch 2')}
+        onClick={() => dispatch(setProgressStep({ progressStep: '2' }))}
       >
         <Typography variant="h5">2</Typography>
       </StyledFab>
       <StyledFab
-        onClick={() => console.log('placeholder for store dispatch 3')}
+        onClick={() => dispatch(setProgressStep({ progressStep: '3' }))}
       >
         <Typography variant="h5">3</Typography>
       </StyledFab>
       <StyledFab
-        onClick={() => console.log('placeholder for store dispatch 4')}
+        onClick={() => dispatch(setProgressStep({ progressStep: '4' }))}
       >
         <Typography variant="h5">4</Typography>
       </StyledFab>
--- src/store/ui/actions.ts
+++ src/store/ui/actions.ts
@@ -1,7 +1,7 @@
 import { ActionType, createAction } from 'typesafe-actions'
 
 export const setProgressStep = createAction('ui/SET_PROGRESS_STEP')<{
-  progressStep: '1' | '2' | '3'
+  progressStep: '1' | '2' | '3' | '4'
 }>()
 
 export const setRoadtripStops = createAction('ui/SET_ROADTRIP_Stops')<{

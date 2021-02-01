86583f67cbd5056566ff8fa64bc88be6ddb1faf4
Jonathan <jonathan@lex.or.at>
2021-02-01 10:33:24 +0100

    feat(containers): structure main page

--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,9 +1,29 @@
 import React, { memo } from 'react'
 
-import { Typography } from '@material-ui/core'
+import { Box, withTheme } from '@material-ui/core'
+import styled from 'styled-components'
+
+import TitleSection from '../../components/TitleSection'
+import RoadtripForm from '../RoadtripForm'
+
+const MainPageStyles = withTheme(styled.div`
+  max-width: 70%;
+  padding: ${(props) => props.theme.spacing(10)}px 0;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-around;
+`)
 
 const MainPage = () => {
-  return <Typography variant="h1">MainPage</Typography>
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center">
+      <MainPageStyles>
+        <TitleSection />
+        <RoadtripForm />
+      </MainPageStyles>
+    </Box>
+  )
 }
 
 export default memo(MainPage)

02e0e26a01289166b9b28ab55ae5dbc436d371aa
Jonathan <jonathan@lex.or.at>
2021-02-03 22:03:42 +0100

    feat(containers): create sign_up page

---
+++ src/containers/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+import { Typography } from '@material-ui/core'
+
+const SignUpPage = () => {
+  return <Typography variant="h1">Anmeldung</Typography>
+}
+
+export default memo(SignUpPage)

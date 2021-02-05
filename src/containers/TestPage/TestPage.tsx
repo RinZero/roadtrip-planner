1504a99a221321701a5ee5b598173785796f49cc
edlingerma <maria.edlinger@gmail.com>
2021-02-05 13:44:54 +0100

    Get json data in component

--- src/containers/TestPage/TestPage.tsx
+++ src/containers/TestPage/TestPage.tsx
@@ -3,6 +3,7 @@ import React, { memo, useEffect } from 'react'
 import { Box, Button, makeStyles, Theme } from '@material-ui/core'
 import styled from 'styled-components'
 
+import CustomCategorySelect from '../../components/CustomCategorySelect'
 import EditRoadtripComponent from '../../components/EditRoadtripComponent'
 import StartGoalForm from '../../components/StartGoalForm'
 import StepsMenu from '../../components/StepsMenu'
@@ -74,7 +75,8 @@ const TestPage = () => {
       <Button className={classes.test}>hello</Button>
       <FancyButton>Heyyyy</FancyButton>
       <TitleSection />
-      <EditRoadtripComponent />
+      {/* <EditRoadtripComponent /> */}
+      <CustomCategorySelect />
     </>
   )
 }

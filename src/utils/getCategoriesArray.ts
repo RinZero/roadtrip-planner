956b823a0364c0fce9a5b6dec5cc885c73902823
edlingerma <maria.edlinger@gmail.com>
2021-02-14 19:55:55 +0100

    added specialcase to second category

--- src/utils/getCategoriesArray.ts
+++ src/utils/getCategoriesArray.ts
@@ -22,27 +22,23 @@ export const getSecondCategories = (first: string) => {
   const second = new Set<{ number: string; name: string }>()
   addEmptyOption(second)
 
-  const subString = first.substring(0, 2)
-
-  for (let i = 0; i < data.length; i++) {
-    const idString = data[i].attributes.id
-    let s = idString[0]
-    let f = first[0]
-    //Specialcase 350 -> check for first 2 digets
-    if (subString === '35') {
-      s = idString.substring(0, 2)
-      f = first.substring(0, 2)
-    }
-
-    if (s === f && idString.length === 4) {
-      // Specialcase 300 -> only 3100, etc not 3510
-      if (first[0] === '3' && first[1] !== '5' && idString[1] === '5') {
-        break
+  if (first[1] === '5') {
+    // when second number is 5 we need to check first two digets of id
+    // Leisure & Outdoor    Natural & Geographical
+    return getSecondCategoriesSpecial(first, second)
+  } else {
+    for (let i = 0; i < data.length; i++) {
+      const idString = data[i].attributes.id
+      if (
+        idString[0] === first[0] &&
+        idString.length === 4 &&
+        idString[1] !== '5'
+      ) {
+        second.add({
+          number: data[i].attributes.id,
+          name: data[i].attributes.name,
+        })
       }
-      second.add({
-        number: data[i].attributes.id,
-        name: data[i].attributes.name,
-      })
     }
   }
   return Array.from(second)
@@ -70,4 +66,23 @@ const addEmptyOption = (set: any) => {
     number: '',
     name: '',
   })
+}
+
+const getSecondCategoriesSpecial = (
+  first: string,
+  second: Set<{ number: string; name: string }>
+) => {
+  const subString = first.substring(0, 2)
+  for (let i = 0; i < data.length; i++) {
+    const idString = data[i].attributes.id
+    const s = idString.substring(0, 2)
+
+    if (s === subString && idString.length === 4) {
+      second.add({
+        number: data[i].attributes.id,
+        name: data[i].attributes.name,
+      })
+    }
+  }
+  return Array.from(second)
 }

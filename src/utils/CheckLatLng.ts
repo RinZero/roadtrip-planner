3ef3326ee43842b831dce6053d3ad0a6af7bd3fb
edlingerma <maria.edlinger@gmail.com>
2021-03-17 21:09:36 +0100

    refactor(utils): deleted useless if in code

--- src/utils/CheckLatLng.ts
+++ src/utils/CheckLatLng.ts
@@ -8,12 +8,6 @@ export const checkDigetInputLng = (event: any) => {
     errorMessage = 'zu groß'
   } else if (num < -90) {
     errorMessage = 'zu klein'
-  } else if (
-    num.match('\\D') &&
-    !num.match('\\D').includes('.') &&
-    !num.match('\\D').includes('-')
-  ) {
-    errorMessage = 'falsches Format'
   } else {
     error = false
   }
@@ -30,12 +24,6 @@ export const checkDigetInputLat = (event: any) => {
     errorMessage = 'zu groß'
   } else if (num < -180) {
     errorMessage = 'zu klein'
-  } else if (
-    num.match('\\D') &&
-    !num.match('\\D').includes('.') &&
-    !num.match('\\D').includes('-')
-  ) {
-    errorMessage = 'falsches Format'
   } else {
     error = false
   }

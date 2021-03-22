5dc3b2fd0dd9224464cbeba0135507d53599bffc
Julia-Katharina Wiesbauer <jwiesbauer.mmt-b2017@fh-salzburg.ac.at>
2021-03-22 18:18:59 +0100

    feat(containers): add react lazy and suspense

--- src/containers/RoadtripForm/RoadtripForm.tsx
+++ src/containers/RoadtripForm/RoadtripForm.tsx
@@ -1,25 +1,35 @@
-import React, { memo } from 'react'
+import React, { memo, Suspense, lazy } from 'react'
 
 import { Typography } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 
-import EditRoadtripComponent from '../../components/EditRoadtripComponent'
-import StartGoalForm from '../../components/StartGoalForm'
-import StepsMenu from '../../components/StepsMenu'
-import SelectCategories from '../../containers/SelectCategories'
+//import EditRoadtripComponent from '../../components/EditRoadtripComponent'
+//import StartGoalForm from '../../components/StartGoalForm'
+//import StepsMenu from '../../components/StepsMenu'
+//import SelectCategories from '../../containers/SelectCategories'
 import { selectProgessStep } from '../../store/selectors'
+const EditRoadtripComponent = React.lazy(
+  () => import('../../components/EditRoadtripComponent')
+)
+const StepsMenu = React.lazy(() => import('../../components/StepsMenu'))
+const StartGoalForm = React.lazy(() => import('../../components/StartGoalForm'))
+const SelectCategories = React.lazy(
+  () => import('../../containers/SelectCategories')
+)
 
 const RoadtripForm = () => {
   const progressStep = useSelector(selectProgessStep())
   return (
     <>
-      <StepsMenu />
-      {progressStep.toString() === '1' && <StartGoalForm />}
-      {progressStep.toString() === '2' && <SelectCategories />}
-      {progressStep.toString() === '3' && <EditRoadtripComponent />}
-      {progressStep.toString() === '4' && (
-        <Typography variant="h3">Danke fürs User Testen xD</Typography>
-      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        <StepsMenu />
+        {progressStep.toString() === '1' && <StartGoalForm />}
+        {progressStep.toString() === '2' && <SelectCategories />}
+        {progressStep.toString() === '3' && <EditRoadtripComponent />}
+        {progressStep.toString() === '4' && (
+          <Typography variant="h3">Danke fürs User Testen xD</Typography>
+        )}
+      </Suspense>
     </>
   )
 }

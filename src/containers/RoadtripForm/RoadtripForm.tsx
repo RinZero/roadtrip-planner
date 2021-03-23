e8ea025b09b91cb965e2d7a803aaf5957a06120b
Jonathan <jonathan@lex.or.at>
2021-03-23 18:17:36 +0100

    Make EditRoadtripComponent to controlled template

--- src/containers/RoadtripForm/RoadtripForm.tsx
+++ src/containers/RoadtripForm/RoadtripForm.tsx
@@ -3,10 +3,10 @@ import React, { memo, Suspense, lazy } from 'react'
 import { Typography } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 
-//import EditRoadtripComponent from '../../components/EditRoadtripComponent'
-//import StartGoalForm from '../../components/StartGoalForm'
-//import StepsMenu from '../../components/StepsMenu'
-//import SelectCategories from '../../containers/SelectCategories'
+import EditRoadtripCreation from '../../components/EditRoadtripCreation'
+import StartGoalForm from '../../components/StartGoalForm'
+import StepsMenu from '../../components/StepsMenu'
+import SelectCategories from '../../containers/SelectCategories'
 import { selectProgessStep } from '../../store/selectors'
 const EditRoadtripComponent = React.lazy(
   () => import('../../components/EditRoadtripComponent')
@@ -21,15 +21,13 @@ const RoadtripForm = () => {
   const progressStep = useSelector(selectProgessStep())
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <StepsMenu />
-        {progressStep.toString() === '1' && <StartGoalForm />}
-        {progressStep.toString() === '2' && <SelectCategories />}
-        {progressStep.toString() === '3' && <EditRoadtripComponent />}
-        {progressStep.toString() === '4' && (
-          <Typography variant="h3">Danke fürs User Testen xD</Typography>
-        )}
-      </Suspense>
+      <StepsMenu />
+      {progressStep.toString() === '1' && <StartGoalForm />}
+      {progressStep.toString() === '2' && <SelectCategories />}
+      {progressStep.toString() === '3' && <EditRoadtripCreation />}
+      {progressStep.toString() === '4' && (
+        <Typography variant="h3">Danke fürs User Testen xD</Typography>
+      )}
     </>
   )
 }

702a3d56a99bd5dd1e345a98e1cecde506c4abab
edlingerma <maria.edlinger@gmail.com>
2021-05-01 19:18:23 +0200

    style(utils): changed base url

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const fetch = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/',
-  // baseURL: 'https://roadtripplaner-backend-develop.herokuapp.com/api/v1/',
+  // baseURL: 'http://localhost:3000/api/v1/',
+  baseURL: 'https://roadtripplaner-backend-develop.herokuapp.com/api/v1/',
 })
 export const fetchUser = (token: string) => {
   return fetch

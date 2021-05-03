155222b41486bed13586796b5c45aada67728ff6
Jonathan <jonathan@lex.or.at>
2021-05-03 13:09:36 +0200

    feat(components): add variable length stopps

--- src/components/StartGoalForm/StartGoalForm.tsx
+++ src/components/StartGoalForm/StartGoalForm.tsx
@@ -14,7 +14,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 import { setProgressStep, setRoadtripStops } from '../../store/actions'
-import { selectUserLocations } from '../../store/selectors'
+import { selectUserLocations, selectRoadtripStops } from '../../store/selectors'
 import { autocomplete, iterateStops } from '../../utils/autocomplete'
 
 const StyledForm = withTheme(styled.form`
@@ -39,13 +39,12 @@ const StyledForm = withTheme(styled.form`
 `)
 
 const StyledButton = withTheme(styled(Button)`
-  width: 80%;
+  width: 100%;
   color: #ffffff;
   background-color: #71b255;
   padding: ${(props) => props.theme.spacing(2)}px;
   border-radius: 15px;
   box-shadow: 0px 3px 6px 1px rgba(0, 0, 0, 0.16);
-  margin-top: ${(props) => props.theme.spacing(3.75)}px;
   &:hover,
   &:active {
     background-color: #355727;
@@ -53,12 +52,10 @@ const StyledButton = withTheme(styled(Button)`
 `)
 
 const AddButton = withTheme(styled(Button)`
-  width: ${(props) => props.theme.spacing(9)}px;
   border-radius: 15px;
   box-shadow: 0px 3px 6px 1px rgba(0, 0, 0, 0.16);
   background-color: #ffffff;
   padding: ${(props) => props.theme.spacing(2)}px;
-  margin: ${(props) => props.theme.spacing(2)}px;
 `)
 
 const StyledTextField = withTheme(styled(TextField)`
@@ -123,6 +120,13 @@ export const StartGoalForm = () => {
   const [showStop, setShowStop] = React.useState(false)
   const onInput = () => setShowStop(true)
 
+  const roadtripStops = useSelector(selectRoadtripStops())
+  const [optionalStops, setOptionalStops] = useState(
+    roadtripStops.slice(0, roadtripStops.length - 1) || [['']]
+  )
+
+  const [activeStop, setActiveStop] = useState(1)
+
   const defaultProps = {
     options: array,
     forcePopupIcon: false,
@@ -176,11 +180,11 @@ export const StartGoalForm = () => {
           />
         </FormBox>
 
+        <Typography variant="h6" align="left">
+          Stops (optional):
+        </Typography>
         <Grid container spacing={1} alignItems="center">
-          <Typography variant="h6" align="left">
-            Stops (optional):
-          </Typography>
-          <FormBox>
+          {/* <FormBox>
             <Autocomplete
               {...defaultProps}
               id="stops[1]"
@@ -218,35 +222,87 @@ export const StartGoalForm = () => {
               />
             ) : null}
             <AddButton onClick={onInput}>+</AddButton>
-          </FormBox>
+          </FormBox> */}
           {message ? <h5>{message}</h5> : null}
-          <Grid item xs={12} lg={4}>
-            <Box p={5}>
-              <StyledButton
-                onClick={async () => {
-                  // get name array with choosen stops
-                  const values = getValues()
-                  // get coordinates array of the stops
-                  const stopArray = await iterateStops(
-                    values.stops,
-                    userLocations
+
+          <Grid item xs={12} lg={8}>
+            <Box
+              component="div"
+              overflow="auto"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              {optionalStops.map((stop, index) => {
+                if (index !== 0 || roadtripStops.length === 0) {
+                  return (
+                    <Box
+                      mx={1}
+                      minWidth={activeStop === index ? '360px' : '70px'}
+                    >
+                      <Box display={activeStop !== index ? 'none' : 'inline'}>
+                        <Autocomplete
+                          {...defaultProps}
+                          id={'stops[' + (index + 1) + ']'}
+                          getOptionLabel={(option) => option}
+                          onInputChange={(event, newInputValue) => {
+                            getItems(newInputValue, event.type)
+                          }}
+                          renderInput={(params) => (
+                            <StartGoalTextField
+                              {...params}
+                              className={active ? 'collapse' : 'expand'}
+                              onKeyDown={toggleClass}
+                              label="Zwischenstopp"
+                              name={'stops[' + (index + 1) + ']'}
+                              inputRef={register}
+                            />
+                          )}
+                        />
+                      </Box>
+                      {activeStop !== index && (
+                        <AddButton onClick={() => setActiveStop(index)}>
+                          {index + 1}
+                        </AddButton>
+                      )}
+                    </Box>
                   )
-                  if (!stopArray[0] || stopArray[0][0] === -1) {
-                    setMessage(
-                      `Anscheinend stimmt was bei deiner Eingabe nicht.
+                }
+              })}
+            </Box>
+          </Grid>
+          <Grid item xs={4} lg={1}>
+            <AddButton
+              onClick={() => setOptionalStops(optionalStops.concat([[]]))}
+            >
+              +
+            </AddButton>
+          </Grid>
+          <Grid item xs={12} lg={3}>
+            <StyledButton
+              onClick={async () => {
+                // get name array with choosen stops
+                const values = getValues()
+                // get coordinates array of the stops
+                const stopArray = await iterateStops(
+                  values.stops,
+                  userLocations
+                )
+                if (!stopArray[0] || stopArray[0][0] === -1) {
+                  setMessage(
+                    `Anscheinend stimmt was bei deiner Eingabe nicht.
                       Start und Ziel müssen ausgefüllt sein und die Orte müssen in Österreich existieren. 
                       Thx.`
-                    )
-                  } else {
-                    setMessage('')
-                    dispatch(setRoadtripStops({ roadtripStops: stopArray }))
-                    dispatch(setProgressStep({ progressStep: '2' }))
-                  }
-                }}
-              >
-                Start
-              </StyledButton>
-            </Box>
+                  )
+                } else {
+                  setMessage('')
+                  dispatch(setRoadtripStops({ roadtripStops: stopArray }))
+                  dispatch(setProgressStep({ progressStep: '2' }))
+                }
+              }}
+            >
+              Start
+            </StyledButton>
           </Grid>
         </Grid>
       </StyledForm>

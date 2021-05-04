3d75733c280e1927115463b7c75c0d1d61930d78
edlingerma <maria.edlinger@gmail.com>
2021-05-04 17:47:15 +0200

    refactor(containers): deleted testpage

--- src/containers/Router/Router.tsx
+++ src/containers/Router/Router.tsx
@@ -10,13 +10,11 @@ import MainPage from '../MainPage'
 import NewPlacePage from '../NewPlacePage'
 import ProfilePage from '../ProfilePage'
 import SignUpPage from '../SignUpPage'
-import TestPage from '../TestPage'
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path="/" component={MainPage} />
-      <Route exact path="/test" component={TestPage} />
       <Route exact path="/neuer_ort" component={NewPlacePage} />
       <Route exact path="/neuer_ort/edit/:id" component={EditPlacePage} />
       <Route exact path="/sign_up" component={SignUpPage} />

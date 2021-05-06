3f86cb30ff3d73e3a208dac0428b36833653b3c2
Julia-Katharina Wiesbauer <jwiesbauer.mmt-b2017@fh-salzburg.ac.at>
2021-05-06 21:08:03 +0200

    feat(roadtripform): add route to shareroadtrip

--- src/containers/RoadtripForm/RoadtripForm.tsx
+++ src/containers/RoadtripForm/RoadtripForm.tsx
@@ -7,6 +7,7 @@ import { selectProgessStep } from '../../store/selectors'
 const EditRoadtripCreation = React.lazy(
   () => import('../../components/EditRoadtripCreation')
 )
+const ShareRoadtrip = React.lazy(() => import('../../containers/ShareRoadtrip'))
 const StepsMenu = React.lazy(() => import('../../components/StepsMenu'))
 const StartGoalForm = React.lazy(() => import('../../components/StartGoalForm'))
 const SelectCategories = React.lazy(
@@ -21,9 +22,7 @@ const RoadtripForm = () => {
       {progressStep.toString() === '1' && <StartGoalForm />}
       {progressStep.toString() === '2' && <SelectCategories />}
       {progressStep.toString() === '3' && <EditRoadtripCreation />}
-      {progressStep.toString() === '4' && (
-        <Typography variant="h3">Der Roadtrip wurde erstellt ^^</Typography>
-      )}
+      {progressStep.toString() === '4' && <ShareRoadtrip />}
     </>
   )
 }

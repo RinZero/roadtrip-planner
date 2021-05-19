b7f20233ed1b609b9031148e2ebebe0f2dc00f94
Julia-Katharina Wiesbauer <jwiesbauer.mmt-b2017@fh-salzburg.ac.at>
2021-05-19 16:06:07 +0200

    test(edit_user): fix edit user test

--- cypress/integration/e2e/edit_user.spec.ts
+++ cypress/integration/e2e/edit_user.spec.ts
@@ -1,35 +1,34 @@
 describe('Edit User Test', () => {
-  it('login and check profile', () => {
+  it('login', () => {
     cy.visit('http://localhost:3001') //wenn Backend läuft 3001
     cy.findByText('LogIn').click()
 
     cy.get('input[name="email"]').type('edit@old.at')
-    cy.get('input[name="password"]').type('edit12345')
+    cy.get('input[name="password"]').type('@Edit12345')
     cy.findByRole('button', { name: 'LogIn' }).click()
+    cy.findByText('SignUp', { timeout: 10000000000 }).should('not.exist')
+  })
 
-    cy.findByLabelText('profile').click()
+  it('check profile', () => {
+    cy.get(`[aria-label="profile"]`).click()
     cy.findByText('Meine Roadtrips:').should('exist')
     cy.findByText('edit@old.at').should('exist')
   })
 
-  it('change name, email and add picture', () => {
+  it('change name and email', () => {
     cy.findByText('edit@old.at').should('exist')
 
     cy.get(`[aria-label="Profil bearbeiten"]`).click()
     cy.get('input[name="userName"]').clear().type('NewEditName')
     cy.get('input[name="email"]').clear().type('new_edit@email.at')
-    cy.get('input[name="password"]').type('edit12345')
-    cy.get('input[name="password_confirmation"]').type('edit12345')
-    cy.get('input[name="picture"]').clear().type('https://img.jpg')
     cy.findByRole('button', { name: 'Speichern' }).click()
 
     cy.findByText('NewEditName').should('exist')
     cy.findByText('new_edit@email.at').should('exist')
-    cy.findByAltText('Profilbild').should('have.attr', 'src', 'https://img.jpg')
   })
 
   it('deletes user', () => {
-    cy.findByText('new_edit@email.at').should('exist')
+    cy.findByText('edit@old.at').should('exist')
 
     cy.get(`[aria-label="Profil bearbeiten"]`).click()
     cy.findByText('Profil löschen').click()
